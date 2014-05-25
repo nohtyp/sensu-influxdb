71331613164bd49ae7f161635b621ef0534f0e80
Thomas Foster <thomas.foster80@gmail.com>
2014-05-24 22:56:04 -0400

    modified sensu handler

--- etc/sensu/handlers/metrics/sensu_influx.rb
+++ etc/sensu/handlers/metrics/sensu_influx.rb
@@ -1,53 +1,49 @@
 #!/usr/bin/env ruby
-##
-## Author: Thomas Foster <thomas.foster80@gmail.com>
-##
-## Released under the same terms as Sensu (the MIT license); see LICENSE
-## for details.
 
-require 'rubygems' if RUBY_VERSION < '1.9.0'
-require 'sensu-handler'
+require 'rubygems'
+require 'json'
 require 'influxdb'
 
-# This class will take event data and send results to influxDB for
-# metric collections
-#
-class Sensutoinfluxdb < Sensu::Handler
-  def filter; end
+# Read event data
+event = JSON.parse(STDIN.read, :symbolize_names => true)
 
-  def handle
-    influxdb_server = settings['influx']['server']
-    influxdb_port   = settings['influx']['port']
-    influxdb_user   = settings['influx']['username']
-    influxdb_pass   = settings['influx']['password']
-    influxdb_db     = settings['influx']['database']
+influxdb_server = "influxdb.familyguy.com"
+influxdb_port   =  8086
+influxdb_user   = 'root'
+influxdb_pass   = 'root'
+influxdb_db     = 'stats'
+data_point      = 'test'
 
-    influxdb_data = InfluxDB::Client.new(influxdb,
-                                         username: influxdb_user,
-                                         password: influxdb_pass,
-                                         port:     influxdb_port,
-                                         server:   influxdb_server
-                                        )
 
-    metrics    = @event['check']['output']
-    data_point = @event['check']['name']
-    host       = @event['client']['name']
-    myip       = @event['client']['address']
+#influxd_server = settings['influx']['server']
+#influxdb_port   = settings['influx']['port']
+#influxdb_user   = settings['influx']['username']
+#influxdb_pass   = settings['influx']['password']
+#influxdb_db     = settings['influx']['database']
 
-    data = []
+influxdb_data = InfluxDB::Client.new influxdb_db, :host => influxdb_server,
+                                                  :username => influxdb_user,
+                                                  :password => influxdb_pass,
+                                                  :port => influxdb_port,
+                                                  :server => influxdb_server
+      
+  mydata = []
+  #puts "#{event[:client][:name]}"
+  event[:check][:output].each do |metric|
+  #puts "#{metric}"
+    m = metric.split
+  next unless m.count == 3
 
-    metrics.split("\n").each do |metric|
-      m = metric.split
+    key = m[0].split('.', 2)[1]
+    #puts "Key: #{key}"
+    key.gsub!('.', '_')
+    value = m[1].to_f
+    #puts "Value: #{value}"
+    mytime = Time.now
 
-      next unless m.count == 3
-
-      key = m[0].split('.', 2)[1]
-      key.gsub!('.', '_')
-      value = m[1].to_f
-
-      data << { host: host, stat: key, value: value, ip: myip }
-    end
+    mydata = {:host => "#{event[:client][:name]}", :check => "#{event[:check][:name]}", 
+              :key => "#{key}", :value => "#{value}", :ip => "#{event[:client][:address]}", 
+              :month => "#{mytime.month}", :day => "#{mytime.day}"}
+    influxdb_data.write_point(data_point, mydata)
   end
 
-  influxdb_data.write_point(data_point, data)
-end
